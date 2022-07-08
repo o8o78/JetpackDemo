c48c382d8823bda3d0e9382a3873d6986939a6f0
zhichao yang <zhichao.yang@backgardon.com>
2022-07-08 15:13:29 +0800

    --JetpackApp获取全局上下文

---
+++ app/src/main/java/com/byteroll/jetpackdemo/JetpackApp.kt
@@ -0,0 +1,19 @@
+package com.byteroll.jetpackdemo
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class JetpackApp: Application() {
+
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+}
