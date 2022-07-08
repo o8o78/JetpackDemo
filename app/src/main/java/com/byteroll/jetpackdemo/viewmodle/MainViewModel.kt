65cb4de77d8b3fc8863335ba011266433fe00bd4
zhichao yang <zhichao.yang@backgardon.com>
2022-07-08 12:26:18 +0800

    --map和switchMap

--- app/src/main/java/com/byteroll/jetpackdemo/viewmodle/MainViewModel.kt
+++ app/src/main/java/com/byteroll/jetpackdemo/viewmodle/MainViewModel.kt
@@ -2,7 +2,10 @@ package com.byteroll.jetpackdemo.viewmodle
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.byteroll.jetpackdemo.bean.Repository
+import com.byteroll.jetpackdemo.bean.User
 
 class MainViewModel(countReserved: Int) : ViewModel(){
 
@@ -21,6 +24,28 @@ class MainViewModel(countReserved: Int) : ViewModel(){
 
     fun clear(){
         _counter.value = 0
+    }
+
+    private var userLiveData = MutableLiveData<User>()
+
+    val userName: LiveData<String> = Transformations.map(userLiveData){
+        user->
+        "${user.firstName}, ${user.lastName}"
+    }
+
+    fun getUser(userId: String): LiveData<User>{
+        return Repository.getUser(userId)
+    }
+
+    private val userIdLiveData = MutableLiveData<String>()
+
+    val user: LiveData<User> = Transformations.switchMap(userIdLiveData){
+        userId->
+        Repository.getUser(userId)
+    }
+
+    fun getUser(userId:String){
+        userIdLiveData.value = userId
     }
 
 }
